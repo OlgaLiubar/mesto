2406bd23b353b02be12b7bccb299deb9ae66cf71
Olga Liubar <olgaliubar@yandex.ru>
2020-12-12 13:50:23 -0600

    feat: open add card popup

--- scripts/index.js
+++ scripts/index.js
@@ -41,9 +41,7 @@ renderGallery();
 
 function composeCard(card){
   const newCard = templateElement.content.cloneNode(true);
-  console.log(newCard);
-
-  captionTitleElement = newCard.querySelector('.card__caption-title');
+  const captionTitleElement = newCard.querySelector('.card__caption-title');
   captionTitleElement.textContent = card.name;
   const cardPhotoElement = newCard.querySelector('.card__photo');
   cardPhotoElement.src = card.link;
@@ -56,18 +54,32 @@ function composeCard(card){
 // для открытия-скрытия попапа
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.popup__close-button');
+// let popupCloseButton = document.querySelector('.popup__close-button');
+
 // для формы
 let popupForm = document.querySelector('.form');
 let nameInput = document.querySelector('.form__input_type_name');
 let jobInput = document.querySelector('.form__input_type_occupation');
 let profileTitle = document.querySelector('.profile__title');
 let profileOccupation = document.querySelector('.profile__occupation');
+const profileAddButton = document.querySelector('.profile__add-button')
 
 // для открытия-скрытия попапа
 
 profileEditButton.addEventListener('click', handleProfileEditButtonClick);
-popupCloseButton.addEventListener('click', handlePopupCloseButtonClick);
+profileAddButton.addEventListener('click', handleProfileAddButtonClick);
+// popupCloseButton.addEventListener('click', handlePopupCloseButtonClick);
+
+
+//добавили слушатель для всех кнопок "закрыть"
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');
+
+popupCloseButtons.forEach(function (button) {
+  button.addEventListener('click', handlePopupCloseButtonClick);
+});
+
+
+
 
 function handleProfileEditButtonClick() {
   popup.classList.add('popup_opened');
@@ -75,8 +87,15 @@ function handleProfileEditButtonClick() {
   jobInput.value = profileOccupation.textContent;
 }
 
-function handlePopupCloseButtonClick() {
-  popup.classList.remove('popup_opened');
+function handleProfileAddButtonClick() {
+  const popupTypeAdd = document.querySelector('.popup_type_add-image');
+  popupTypeAdd.classList.add('popup_opened');
+}
+
+
+function handlePopupCloseButtonClick(evt) {
+  const targetItem = evt.target.closest('.popup');
+  targetItem.classList.remove('popup_opened');
 }
 
 // для отправки формы
@@ -86,6 +105,9 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileOccupation.textContent = jobInput.value;
-    handlePopupCloseButtonClick();
+    handlePopupCloseButtonClick(evt);
   }
+
+
+
 
