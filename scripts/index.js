2601f18ec436b82e7fa704d9a455d3ba767055af
Olga Liubar <olgaliubar@yandex.ru>
2020-12-12 09:45:13 -0600

    render initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,58 @@
+//массив для изначальных карточек
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const galleryContainerElement = document.querySelector('.gallery__elements'); 
+const templateElement = document.querySelector('.gallery-template');
+
+
+
+function renderGallery() {
+  const galleryItems = initialCards.map(composeCard);
+  galleryContainerElement.append(...galleryItems);
+}
+
+renderGallery();
+
+function composeCard(card){
+  const newCard = templateElement.content.cloneNode(true);
+  console.log(newCard);
+
+  captionTitleElement = newCard.querySelector('.card__caption-title');
+  captionTitleElement.textContent = card.name;
+  const cardPhotoElement = newCard.querySelector('.card__photo');
+  cardPhotoElement.src = card.link;
+  return newCard;
+}
+
+
+
+
 // для открытия-скрытия попапа
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
