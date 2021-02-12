d81dd372844764b397cc524bd7bd1ea835b60348
Olga Liubar <olgaliubar@yandex.ru>
2021-02-12 04:55:14 +0300

    feat/js and css moved to pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import './index.css';
 import {
   initialCards,
   config
-} from './utils/constants.js';
-import Card from './components/Card.js';
-import FormValidator from './components/FormValidator.js';
-import Section from './components/Section.js';
-import PopupWithImage from './components/PopupWithImage.js';
-import PopupWithForm from './components/PopupWithForm.js';
-import UserInfo from './components/UserInfo.js';
+} from '../utils/constants.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 const galleryContainerElement = document.querySelector('.gallery__elements');
 
